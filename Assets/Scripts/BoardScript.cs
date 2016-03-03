39b39fd862ec95e0db1cfdd9cdf796fb04d92668
CSUCI <csuci@Joaos-MacBook-Pro.local>
2016-03-02 20:30:37 -0800

    Josh is a beast

---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardScript : MonoBehaviour 
+{
+	public GameObject cube;
+	public int size = 20;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for(int r = 0; r < size; r++)
+		{
+			for(int c = 0; c < size; c++)
+			{
+				
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
