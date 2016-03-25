4a7ae8dc845a1590035909355ee17c656e917e45
CSUCI <csuci@Joaos-MacBook-Pro.local>
2016-03-24 21:11:20 -0700

    Grid loads on server

--- Assets/Assets/Scripts/Alt/Network/Server/Server.cs
+++ Assets/Assets/Scripts/Alt/Network/Server/Server.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Server : NetworkManager {
 
@@ -10,16 +11,12 @@ public class Server : NetworkManager {
 	public GameObject UserPrefab;
 
 
-	public override void OnStartServer(){
-	}
+	public List <List<Tile>> map = new List<List<Tile>>();
+	public int mapSize = 13;
 
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public override void OnStartServer(){
+
+
 	}
 
 	private void spawnObject(GameObject prefab){
@@ -27,6 +24,39 @@ public class Server : NetworkManager {
 		GameObject obj = GameObject.Instantiate(prefab);
 		NetworkServer.Spawn(obj);
 	
+	}
+
+	public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
+	{
+
+		Debug.Log ("Player has been added!");
+
+		base.OnServerAddPlayer (conn, playerControllerId);
+	}
+
+	public override void OnStartHost ()
+	{
+
+		generateMap ();
+
+
+
+		base.OnStartHost ();
+	}
+
+	private void generateMap(){
+
+		for (int i = 0; i < mapSize; i++) {
+			List <Tile> row = new List<Tile> ();
+			for (int j = 0; j < mapSize; j++) {
+				//spawn grid
+				GameObject t = Instantiate (TilePrefab, new Vector3 (i - Mathf.Floor (mapSize / 2), 0, -j + Mathf.Floor (mapSize / 2)), Quaternion.Euler (new Vector3 ())) as GameObject;
+				Tile tile = t.GetComponent<Tile>();
+				tile.gridPosition = new Vector2 (i, j);
+				row.Add (tile);
+			}
+		}
+
 	}
 
 
